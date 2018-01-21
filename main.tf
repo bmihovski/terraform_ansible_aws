4501665b7b50aee2cbbb36038e66b0891186428d
bmihovski <bmihovski@live.com>
2018-01-21 16:35:37 +0200

    Squashed commit of the following:
    commit 88b19d1041e2c2f8f05437a166eaf0bec9a95469
    Author: bmihovski <bmihovski@live.com>
    Date:   Sun Jan 21 16:35:20 2018 +0200
    
    	Dev instance and calling ansible playbook implemented

--- main.tf
+++ main.tf
@@ -277,7 +277,32 @@ resource "aws_key_pair" "auth" {
   public_key = "${file(var.public_key_path)}"
 }
 #dev server
-  #ansible play
+resource "aws_instance" "dev" {
+  instance_type= "${var.dev_instance_type}"
+  ami = "${var.dev_ami}"
+  tags {
+    Name = "dev"
+  }
+
+  key_name = "${aws_key_pair.auth.id}"
+  vpc_security_group_ids = ["${asw_security_group.public.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
+  subnet_id = "${aws_subnet.public.id}"
+  
+  provisioner "local-exec" {
+    command = "cat <<EOF > aws_hosts
+[dev]
+${aws_instance.dev.public_ip}
+[dev:vars]
+s3code=${aws_s3_bucket.code.bucket}
+EOF"
+  }
+  #ansible play  
+  provisioner "local-exec" {
+    command = "sleep 6m && ansible-playbook -i aws_hosts wordpress.yml"
+  }
+}
+
 #load balancer
 #AMI
 #launch
