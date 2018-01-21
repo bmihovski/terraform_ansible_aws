053a140d33633f85b41bcb7567fcecf226c20b87
bmihovski <bmihovski@live.com>
2018-01-21 14:30:20 +0200

    Squashed commit of the following:
    commit 9efe1ec6977bc80deb85fb9a513455f0bbaccefd
    Author: bmihovski <bmihovski@live.com>
    Date:   Sun Jan 21 14:29:53 2018 +0200
    
    	Subnet associations and security groups are implemented

--- main.tf
+++ main.tf
@@ -99,12 +99,97 @@ resource "aws_subnet" "rds3" {
     Name = "rds3"
   }
 }
+#Subnet Associations
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private1_assoc" {
+  subnet_id = "${aws_subnet.private1.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private2_assoc" {
+  subnet_id = "${aws_subnet.private2.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_db_subnet_group" "rds_subnetgroup" {
+  name = "rds_subnetgroup"
+  subnet_ids = ["${aws_subnet.rds1.id}", "${aws_subnet.rds2.id", "${aws_subnet.rd3.id}"]
+    
+  tags {
+    Name = "rds_sng"
+  }
+}
+    
 #Security groups
+#Public
+resource "aws_security_group" "public" {
+  name = "sg_public"
+  description = "Used for public and private instances for load balancer access"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  #SSH
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+  #HTTP
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port = 0
+    to_port =0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 #Private
-#Public
+resource "aws_security_group" "private" {
+  name = "sg_private"
+  description = "Used for private instances"
+  vpc_id = "${aws_vpc.vpc.id}"
+  
+#Access from other security groups
+  
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["10.1.0.0/16"]
+  }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
+}
 #RDS
-
+resource "aws_security_group" "RDS" {
+  name = "sg_rds"
+  description = "Used for DB instances"
+  vpc_id = "${aws_vpc.vpc.id}"
+  
+#SQL access from public/private security group
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
+  } 
+}
 #S3 code bucket
 
 #compute
