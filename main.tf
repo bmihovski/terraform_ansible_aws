e47e1ad66536c0a361b331392235a3738738a55a
bmihovski <bmihovski@live.com>
2018-01-21 18:32:43 +0200

    Squashed commit of the following:
    commit a31ffb1c3447f1b1aec3fe00d0529a7658253335
    Author: bmihovski <bmihovski@live.com>
    Date:   Sun Jan 21 18:32:32 2018 +0200
    
    	route 53 records are implemented

--- main.tf
+++ main.tf
@@ -394,6 +394,36 @@ resource "aws_autoscaling_group" "asg" {
 }
 #Route53
 #primary zone
+resource "aws_route53_zone" "primary" {
+  name = "${var.domain_name}.com"
+  delegation_set_id = "${var.delegation_set}"
+}
 #www
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "www.${var.domain_name}.com"
+  type = "A"
+  
+  alias {
+    name = "${aws_elb.prod.dns_name}"
+    zone_id = "${aws_elb.prod.zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 #dev
+resource "aws_route53_record" "dev" {
+  zone_id = "${aws_route53_zone.primary.zone_id"
+  name = "dev.${var.domain_name}.com"
+  type = "A"
+  ttl = "300"
+  records = ["${aws_instance.dev.public_ip}"]
+}
 #db
+resource "aws_route53_record" "db" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "db.${var.domain_name}.com"
+  type = "CNAME"
+  ttl = "300"
+  records = ["${aws_db_instance.db.address}"]
+}
