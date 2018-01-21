47deec2da17506b8ffd28b407c08974844cd2425
bmihovski <bmihovski@live.com>
2018-01-21 12:16:42 +0200

    Squashed commit of the following:
    commit beee4ec138536a3ce3f349a75c0f553ffcd719ac
    Author: bmihovski <bmihovski@live.com>
    Date:   Sun Jan 21 12:16:29 2018 +0200
    
    	Project plan created

--- main.tf
+++ main.tf
@@ -2,3 +2,41 @@ privider "aws" {
   region = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
+
+#IAM
+S3_access
+
+#VPC
+#Internet Gateway
+#Public route table
+#Private route
+#Subnets
+#public
+#private 1
+#private 2
+#RDS1
+#RDS2
+#RDS3
+
+#Security groups
+
+#Private
+#Public
+#RDS
+
+#S3 code bucket
+
+#compute
+#key pair
+#dev server
+  #ansible play
+#load balancer
+#AMI
+#launch
+#ASG
+
+#Route53
+#primary zone
+#www
+#dev
+#db
